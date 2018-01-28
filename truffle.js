8bb8ecce1e6ab75c305c8ecc1d7dfa6a94ad1649
Wellington <wellingtonmb88@gmail.com>
2018-01-28 09:39:44 -0200

    Add framework zeppelin-solidity to the project

--- truffle.js
+++ truffle.js
@@ -1,4 +1,14 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+      gas: 6712388,
+      gasPrice: 65000000000,
+      // from: "0xf17f52151EbEF6C7334FAD080c5704D77216b732"
+    }
+  }
 };
